de7f7d1d3dd4dc83fd2debd11d1295cd5202ebca
mayarakaren <mayarakaren5910@gmail.com>
2024-03-15 16:58:09 -0300

    feat: resultImc

--- src/components/form/resultImc/resultImc.js
+++ src/components/form/resultImc/resultImc.js
@@ -2,3 +2,13 @@ import React, { useState } from "react";
 import { View, Text} from "react-native";
 import stylesResult from "./styleResult";
 
+export default function ResultImc(props){
+    const [reset, setReset] = useState(false);
+    let message;
+    let color;
+    let belowWeightColor = "#18FFB5";
+    let normalWeightColor = "#20FF23";
+    let aboveWeightColor = "#F2661F";
+    
+
+}
